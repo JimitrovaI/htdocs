2a73c45ae23da8c6d78ab1ff53ac225f2ea0919b
programer <86601736+JimitrovaI@users.noreply.github.com>
2021-12-23 00:15:56 +0900

    no_mean2

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,29 @@
+<?php
+
+class MY_Controller extends CI_Controller
+{
+
+    protected $langs = array();
+
+    public function __construct()
+    {
+
+        parent::__construct();
+		$this->load->library('mailer');
+		$this->load->model('business_model');
+		$this->load->model('transactions_model');
+
+        if ($this->session->has_userdata('language')) {
+            $language = $this->session->userdata('language');
+        } else {
+            $language = 'English';
+        }
+
+        $this->config->set_item('language', $language);
+        $lang_array = array('form_validation_lang');
+        $lang_array[] = 'app_files/system_lang';
+
+        $this->load->language($lang_array, $language);
+    }
+
+}
